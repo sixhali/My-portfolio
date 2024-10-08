9f47b4c71121c9611fbe2f96d230a2177a469d22
sixhali <bayram43kocaman@gmail.com>
2024-10-08 15:56:04 +0400

    Realy proje aded updated

--- src/Components/MyProje/MyProje.jsx
+++ src/Components/MyProje/MyProje.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import MyProjeModule from './MyProje.module.css'
 import myProje1 from '../../assets/MyProje_1.png';
-import myProje2 from '../../assets/MyProje_2.jpg';
-import myProje3 from '../../assets/MyProje_3.jpg';
+import myProje2 from '../../assets/MyProje_2.png';
+import myProje3 from '../../assets/MyProje_3.png';
 import myProje4 from '../../assets/MyProje_4.png';
-import myProje5 from '../../assets/MyProje_5.png';
-import myProje6 from '../../assets/MyProje_6.png';
 
 
 
@@ -15,12 +13,10 @@ function MyProje({isDarkMode}) {
         <div className={MyProjeModule.MyProjeBoxs}>
            <h1 style={{ color: isDarkMode ? '#fff' : '#000' }}>My Proje</h1>
             <div className={MyProjeModule.MyProjeBox}>
-                <a href="#"><img src={myProje1} width={390}/></a>
-                <a href="#"><img src={myProje2} width={416}/></a>
-                <a href="#"><img src={myProje3} width={400}/></a>
-                <a href="#"><img src={myProje4} width={416}/></a>
-                <a href="#"><img src={myProje5} width={416}/></a>
-                <a href="#"><img src={myProje6} width={410}/></a>
+                <a href="https://susha-logostic-react-js.vercel.app/" target='_blank'><img src={myProje1} width={416}/></a>
+                <a href="https://traveling-blog-website.vercel.app/" target='_blank'><img src={myProje2} width={416}/></a>
+                <a href="https://baku-devs-figma-proje.vercel.app/" target='_blank'><img src={myProje3} width={416}/></a>
+                <a href="https://archbaku-figma-proje.vercel.app/" target='_blank'><img src={myProje4} width={416}/></a>
             </div>
 
         </div>
